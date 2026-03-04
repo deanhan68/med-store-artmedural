e12c1b4d19da960fc2f3e9b4c08b9db24d8c70f2
deanhan68 <savelynikolaev@mail.ru>
2026-03-04 19:42:39 +0500

    Сверстана основаня часть фильтрации

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,45 @@
-import { Container, Title } from "@/components/shared";
+import { Categories, CheckboxFiltersGroup, Container, Filters, SortPopup, Title } from "@/components/shared";
+import { TopBar } from "@/components/shared/top-bar";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-
     <>
-    <Container className="mt-10">
-      <Title text="Вся продукция" size="lg" className="font-extrabold"/>
+      <Container className="mt-8">
+        <Title text="Вся продукция" size="lg" className="font-extrabold"/>
+      </Container>
 
- 
-    </Container>
-    
+      <TopBar/>
+
+      <Container className="mt-8 pb-14">
+        <div className="flex gap-[60px]">
+
+          {/* Фильтрация */}
+          <div className="x-[250px]">
+            <Filters/>
+          </div>
+
+
+
+          {/* Список товаров */}
+          <div className="flex-1">
+            <div className="flex flex-col gap-16">
+              Список товаров
+            </div>
+          </div>
+
+
+
+        </div>
+       
+        
+      </Container>
+     
+
+     
+
+      
      </>
   )
 
---
+++ components/shared/filters.tsx
@@ -0,0 +1,99 @@
+'use client'
+
+import React from 'react';
+import { Title } from './title';
+import { FilterCheckbox } from './filter-checkbox';
+import { Input } from '../ui/input';
+import { Slider } from '../ui';
+import { RangeSlider } from './range-slider';
+import { CheckboxFiltersGroup } from './checkbox-filters-group';
+
+
+interface Props {
+  className?: string;
+}
+
+export const Filters: React.FC<Props> = ({ className }) => {
+  return (
+   <div className={className}>
+        <Title text="Фильтрация" size="sm" className="mb-5 font-bold" />
+
+        {/* Вверхние чекбоксы */}
+
+        <div className="flex flex-col gap-4">
+            <FilterCheckbox text="Стерильные" value="1" />
+            <FilterCheckbox text="Нестерильные" value="2" />
+        </div>
+
+        {/* Фильтр цен */}
+
+        <div className="mt-5 border-y border-y-neutral-100 py-6 pb-7">
+            <p className="font-bold mb-3">Цена от и до:</p>
+            <div className="flex gap-3 mb-5">
+            <Input type="number" placeholder="300" min={0} max={30000} defaultValue={0} />
+            <Input type="number" min={0} max={50000} placeholder="30000" />
+            </div>
+            <RangeSlider min={0} max={50000} step={10} value={[0, 50000]} />
+        </div>
+
+        
+        <CheckboxFiltersGroup
+        title="Производитель:"
+        className="mt-5"
+        limit={6}
+        defaultItems={[
+          {
+            text: 'Винар',
+            value: '1',
+          },
+          {
+            text: 'ВИТА-ПУЛ',
+            value: '2',
+          },
+          {
+            text: 'КлиниПак',
+            value: '3',
+          },
+          {
+            text: 'Медитек',
+            value: '4',
+          },
+          {
+            text: 'Навтекс',
+            value: '5',
+          },
+          {
+            text: 'Ньюфарм',
+            value: '6',
+          },
+        ]}
+        items={[
+          {
+            text: 'Винар',
+            value: '1',
+          },
+          {
+            text: 'ВИТА-ПУЛ',
+            value: '2',
+          },
+          {
+            text: 'КлиниПак',
+            value: '3',
+          },
+          {
+            text: 'Медитек',
+            value: '4',
+          },
+          {
+            text: 'Навтекс',
+            value: '5',
+          },
+          {
+            text: 'Ньюфарм',
+            value: '6',
+          },
+        ]}
+      />      
+   </div>
+  );
+};
