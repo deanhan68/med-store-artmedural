e5c20c0df4bce8404ef1b4874ccf87345e95963f
deanhan68 <savelynikolaev@mail.ru>
2026-03-09 12:33:37 +0500

    Доработка категорий

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,12 @@ import { ProductCard } from "@/components/shared/product-card";
 import { ProductsGroupList } from "@/components/shared/product-group-list";
 import { TopBar } from "@/components/shared/top-bar";
 import { Button } from "@/components/ui/button";
+import { ImageUp } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
   const newLocal = "flex flex-col gap-16";
+
   return (
     <>
       <Container className="mt-8">
@@ -16,10 +18,10 @@ export default function Home() {
       <TopBar/>
 
       <Container className="mt-8 pb-14">
-        <div className="flex gap-[60px]">
+        <div className="flex gap-[80px]">
 
           {/* Фильтрация */}
-          <div className="x-[250px]">
+          <div className="w-[250px]">
             <Filters/>
           </div>
 
@@ -27,23 +29,49 @@ export default function Home() {
 
           {/* Список товаров */}
           <div className="flex-1">
-            <div className={newLocal}>
+            <div className="flex flex-col gap-16">
+              <ProductsGroupList title="Гигиена" items={[
+                {
+                  id: 1,
+                  name: "BENOVY LATEX CHLORINATED",
+                  imageUrl: "http://artmedural.ru/image/cache/catalog/tovar/t02/t21/t311/fffc20728b52ed9761d3903053422c17-1100x1100.png",
+                  price: 550,
+                  items: [{price: 550}],
+                },
+                {
+                  id: 1,
+                  name: "BENOVY LATEX CHLORINATED",
+                  imageUrl: "http://artmedural.ru/image/cache/catalog/tovar/t02/t21/t311/fffc20728b52ed9761d3903053422c17-1100x1100.png",
+                  price: 550,
+                  items: [{price: 550}],
+                },
+                ]} 
+                  categoryId={1} />
+
+                <ProductsGroupList title="Контроль продуктов питания" items={[
+                {
+                  id: 1,
+                  name: "Молконт-рН",
+                  imageUrl: "http://artmedural.ru/image/cache/catalog/tovar/t08/312----800x1000-1100x1100h.jpg",
+                  price: 350,
+                  items: [{price: 350}],
+                },
+                {
+                  id: 1,
+                  name: "Молконт-рН",
+                  imageUrl: "http://artmedural.ru/image/cache/catalog/tovar/t08/312----800x1000-1100x1100h.jpg",
+                  price: 350,
+                  items: [{price: 350}],
+                },
+                
+
+                
+                ]} 
+                  categoryId={1} />
             </div>
           </div>
-
-          <ProductsGroupList title={"Гигиена и дезинфекция"} items={[1,1,1]}/>
-
-
-
         </div>
-       
-        
       </Container>
-     
-
-     
-
-      
      </>
   )
 
