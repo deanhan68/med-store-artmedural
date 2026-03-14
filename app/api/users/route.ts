0fabe444015b3afc589716da73a76e3c59876054
deanhan68 <savelynikolaev@mail.ru>
2026-03-14 12:03:49 +0500

    Создал полностью готовые модели для работы с бекендом

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -3,6 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET() {
     // SELECT FROM users WHERE email = 'emasd'
+
     const users = await prisma.user.findMany();
 
     return NextResponse.json(users)
