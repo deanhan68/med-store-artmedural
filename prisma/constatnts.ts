2cfaade867cf457ea9079b2f6f65fc35abeb8ba4
deanhan68 <savelynikolaev@mail.ru>
2026-03-17 18:07:07 +0500

    Сделаны основные таблицы и расставлен связи

---
+++ prisma/constatnts.ts
@@ -0,0 +1,100 @@
+export const categories = [
+    {
+        name: "Гигиена"
+    },
+    {
+        name: "Медецинская одежда"
+    },
+    {
+        name: "Контроль"
+    },
+    {
+        name: "Перевязочные материалы"
+    },
+    {
+        name: "Документация"
+    },
+    {
+        name: "Стериализация"
+    },
+
+    
+]
+
+export const countProduct = [
+    {
+        name: "x1",
+        price: 100,
+    },
+    {
+        name: "x2",
+        price: 300,
+    },
+    {
+        name: "x5",
+        price: 300,
+    },
+    {
+        name: "x10",
+        price: 300,
+    },    
+].map((obj, index) => ({id: index + 1, ...obj}));
+
+
+export const products = [
+    {
+        name:"Маски для лица",
+        imageUrl:"http://artmedural.ru/image/cache/catalog/tovar/t02/t29/mav_4004-1100x1100w.jpg",
+        categoryId: 2,
+    },
+    {
+        name:"Шапочка клип-берет Шарлотта",
+        imageUrl:"http://artmedural.ru/image/cache/catalog/tovar/t15/shapka_ntk_tovar-1100x1100h.jpg",
+        categoryId: 2,
+    },
+    {
+        name:"Молконт-ЧАС",
+        imageUrl:"http://artmedural.ru/image/cache/catalog/tovar/t08/molkont-chas-1100x1100h.png",
+        categoryId: 3,
+    },
+    {
+        name:"БиоТЕСТ-ПЛАЗМА-ВИНАР",
+        imageUrl:"http://artmedural.ru/image/cache/catalog/tovar/t07/760847c3b7be506384c02d542f951060-1100x1100w.jpg",
+        categoryId: 3,
+    },
+    {
+        name:"Эластичный бинт",
+        imageUrl:"http://artmedural.ru/image/cache/catalog/tovar/t02/t29/img_0008_2-1100x1100w.jpg",
+        categoryId: 4,
+    },
+    {
+        name:"Вата гигиеническая",
+        imageUrl:"http://artmedural.ru/image/cache/catalog/tovar/t02/t29/mav_3902-1100x1100w.jpg",
+        categoryId: 4,
+    },
+    {
+        name:"Журнал контроля концентраций рабочих растворов дезинфицирующих и стерилизующих средств",
+        imageUrl:"http://artmedural.ru/image/cache/catalog/tovar/t08/c1dbd2a2-1100x1100.jpg",
+        categoryId: 5,
+    },
+    {
+        name:"Журнал регистрации и контроля ультрафиолетовой бактерицидной установки",
+        imageUrl:"http://artmedural.ru/image/cache/catalog/tovar/t08/d509449f377be1db4c23fc9072b18f74-1100x1100w.jpg",
+        categoryId: 5,
+    },
+    {
+        name:"Бумага крепированная (стандартная/ мягкая/ усиленная) КЛИНИПАК",
+        imageUrl:"http://artmedural.ru/image/cache/catalog/tovar/t04/8249695-1100x1100.jpeg",
+        categoryId: 6,
+    },
+    {
+        name:"Лента липкая для стерилизации, «СтериТ®»",
+        imageUrl:"http://artmedural.ru/image/cache/catalog/tovar/t05/lenta_lipkaja_dlja_vozdushnoj_sterilizacii-1100x1100.png",
+        categoryId: 6,
+    },
+
+    
+
+
+
+]
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,14 +1,181 @@
 import { Prisma, PrismaClient } from '@prisma/client';
 import { prisma } from './prisma-client';
+import { Verified } from 'lucide-react';
+import {hashSync} from 'bcrypt';
+import { categories, countProduct, products } from './constatnts';
+
+const randomNumber = (min: number, max: number) => {
+  return Math.floor(Math.random() * (max - min) + min);
+};
 
 
+const generateProductItem = ({ 
+  productId,
+  productType,
+  size,
+}: {
+  productId: number,
+  productType?: 1 | 2, // стерильные | не стерильыне
+  size?: 200 | 500 | 1000 | 2500 | 5000, // объем
+}
+) => {
+  return {
+    productId,
+    price:randomNumber(190, 600),
+    productType,
+    size,
+
+  } as Prisma.ProductItemUncheckedCreateInput;
+}
 
 async function up() {
+  // Добавляем вызов метода Prisma, чтобы данные улетели в базу
+  await prisma.user.createMany({
+    data: [
+      {
+        fullName: 'User',
+        email: 'user@user.ru',
+        password: hashSync('11111', 10),
+        verified: new Date(),
+        role: 'USER',
+      },
+      {
+        fullName: 'Admin',
+        email: 'admin@admin.ru',
+        password: hashSync('11111', 10),
+        verified: new Date(),
+        role: 'ADMIN',
+      },
+    ]
+  });
 
 
+  await prisma .category.createMany({
+    data: categories,
+  });
+
+  await prisma .countProduct.createMany({
+    data: countProduct,
+  });
+
+  await prisma .product.createMany({
+    data: products,
+  });
+
+
+  const gigiena1 = await prisma.product.create({
+    data: {
+      name:'АВАНСЕПТ АКТИВ',
+      imageUrl: 
+      'http://artmedural.ru/image/cache/catalog/tovar/t02/t28/xavansept-aktiv-1-litr.jpg.pagespeed.ic.ycekifye05-1100x1100w.jpg',
+      categoryId: 1,
+      countProduct : {
+        connect: countProduct.slice(0, 5)
+      },
+    }
+  })
+
+  const gigiena2 = await prisma.product.create({
+    data: {
+      name:'Кутасепт Ф ',
+      imageUrl: 
+      'http://artmedural.ru/image/cache/catalog/tovar/t02/t28/680f8a4a9c911b5b9af5e551c02cb10d-1100x1100.jpg',
+      categoryId: 1,
+      countProduct : {
+        connect: countProduct.slice(0, 5)
+      },
+    }
+  })
+
+  const gigiena3 = await prisma.product.create({
+    data: {
+      name:'Фолицид Ультра',
+      imageUrl: 
+      'http://artmedural.ru/image/cache/catalog/tovar/t02/t28/6f61b3a50f24e6a33ddc2d1f949e743b-1100x1100.jpg',
+      categoryId: 1, 
+      countProduct : {
+        connect: countProduct.slice(0, 5)
+      },
+    }
+  })
+  await prisma.productItem.createMany({
+    data :[
+
+      // АВАНСЕПТ АКТИВ
+      generateProductItem ({productId: gigiena1.id, productType: 1, size: 1000,}), // стерильные
+      generateProductItem ({productId: gigiena1.id, productType: 1, size: 500,}), // стерильные
+
+      // Кутасепт Ф
+      generateProductItem ({productId: gigiena2.id, productType: 2, size: 5000,}), // не стерильный
+
+      // Фолицид Ультра
+      generateProductItem ({productId: gigiena3.id, productType: 1, size: 200,}),// стерильные
+      generateProductItem ({productId: gigiena3.id, productType: 1, size: 500,}), // стерильные
+      generateProductItem ({productId: gigiena3.id, productType: 1, size: 1000,}), // стерильные
+
+      // Остальные продукты
+      generateProductItem ({productId:1}),
+      generateProductItem ({productId:2}),
+      generateProductItem ({productId:3}),
+      generateProductItem ({productId:4}),
+      generateProductItem ({productId:5}),
+      generateProductItem ({productId:6}),
+      generateProductItem ({productId:7}),
+      generateProductItem ({productId:8}),
+      generateProductItem ({productId:9}),
+      generateProductItem ({productId:10}),
+      
+
+
+
+
+
+
+    ],
+  })
+
+
+  await prisma.cart.createMany({
+    data : [
+      {
+        userId: 1,
+        totalAmount: 0,
+        token: "111111",
+      },
+      {
+        userId: 2,
+        totalAmount: 0,
+        token: "22222",
+      },
+    ]
+  })
+
+
+  await prisma.cartItem.create({
+    data : {
+        productItemId: 1,
+        cartId:1,
+        quantity: 2,
+        countProduct: {
+          connect: [{id: 1}, {id: 2}, {id: 3 }, {id: 4}]
+      },
+    },
+  });
+  
+  
+  console.log('Данные успешно засеяны! 🌱');
 }
 
 async function down() {
+
+  await prisma.$executeRaw`TRUNCATE TABLE "User" RESTART IDENTITY CASCADE`;
+  await prisma.$executeRaw`TRUNCATE TABLE "Category" RESTART IDENTITY CASCADE`;
+  await prisma.$executeRaw`TRUNCATE TABLE "Product" RESTART IDENTITY CASCADE`;
+  await prisma.$executeRaw`TRUNCATE TABLE "ProductItem" RESTART IDENTITY CASCADE`;
+  await prisma.$executeRaw`TRUNCATE TABLE "CountProduct" RESTART IDENTITY CASCADE`;
+  await prisma.$executeRaw`TRUNCATE TABLE "Cart" RESTART IDENTITY CASCADE`;
+  await prisma.$executeRaw`TRUNCATE TABLE "CartItem" RESTART IDENTITY CASCADE`;
+  
     
 }
 
@@ -20,3 +187,14 @@ async function main() {
         console.error(e);
       }
 }
+
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
