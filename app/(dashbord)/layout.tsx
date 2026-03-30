80b9e70ab117d6ea85d83a7e19984908ad8ed114
deanhan68 <savelynikolaev@mail.ru>
2026-03-30 14:19:25 +0500

    разделил приложение на две ветки для будущего дашборда

---
+++ app/(dashbord)/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Nunito } from "next/font/google";
+import "../globals.css"
+import { Header } from "@/components/shared/header";
+
+const nunito = Nunito({
+  variable: "--font-nunito",
+  subsets: ["cyrillic"],
+  weight:['400', '500', '600','700','800', '900']
+});
+
+
+
+export const metadata: Metadata = {
+  title: "ArtMedUral | Главная",
+};
+
+export default function DashboardLayout({ children, }: Readonly<{ children: React.ReactNode;}>) {
+  return (
+    <html lang="en">
+      <body
+        className={nunito.className}>
+          <main className="min-h-screen">
+
+            {children}
+          </main>
+
+      </body>
+    </html>
+  );
+}
