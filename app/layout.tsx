ce17068fa6615deff7c88cd8a0286d3f4e8d4c9e
deanhan68 <savelynikolaev@mail.ru>
2026-05-17 14:20:28 +0500

    Final fix for payment auth 5

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,14 +3,11 @@ import { Nunito } from "next/font/google";
 import "./globals.css"
 import { Providers } from "@/shared/components/shared/providers";
 
-
-
 const nunito = Nunito({
   variable: "--font-nunito",
   subsets: ["cyrillic"],
-  weight:['400', '500', '600','700','800', '900']
+  weight: ['400', '500', '600', '700', '800', '900']
 });
-
 
 export default function RootLayout({
   children,
@@ -20,9 +17,10 @@ export default function RootLayout({
   return (
     <html lang="en">
         <head>
-            <link data-rh="true" rel="icon" href="/logo.png"></link>
+            <link data-rh="true" rel="icon" href="/logo.png" />
         </head>
-        <body className={nunito.className}>
+        {/* Добавили max-w-[100vw] и overflow-x-hidden прямо к шрифту */}
+        <body className={`${nunito.className} max-w-[100vw] overflow-x-hidden antialiased`}>
           <Providers>{children}</Providers>
         </body>
     </html>
